16d22bc4ab9008fbc042887faa6b7a615dbef6a7
Felipe Mantovani <92172133+F-Mantovani@users.noreply.github.com>
2022-09-17 01:41:58 +0200

    testing preview

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 import { LoginForm } from './components/login-signup-forms/LoginForm';
 import { SignUpForm } from './components/login-signup-forms/SignUpForm';
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 
 function App() {
